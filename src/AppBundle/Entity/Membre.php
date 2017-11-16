343abfc8162c5ccc4c69c18218ce6f4332812794
Adrien Thevenet <adrien12.thevenet@gmail.com>
2017-11-16 16:19:30 +0100

    membre BDD

---
+++ src/AppBundle/Entity/Membre.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Membre
+ *
+ * @ORM\Table(name="membre")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MembreRepository")
+ */
+class Membre
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Promotion", type="string", length=255)
+     */
+    private $promotion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Role", type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="Nb_Connexion", type="integer")
+     */
+    private $nbConnexion;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Membre
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Membre
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Membre
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set promotion
+     *
+     * @param string $promotion
+     *
+     * @return Membre
+     */
+    public function setPromotion($promotion)
+    {
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+
+    /**
+     * Get promotion
+     *
+     * @return string
+     */
+    public function getPromotion()
+    {
+        return $this->promotion;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     *
+     * @return Membre
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set nbConnexion
+     *
+     * @param integer $nbConnexion
+     *
+     * @return Membre
+     */
+    public function setNbConnexion($nbConnexion)
+    {
+        $this->nbConnexion = $nbConnexion;
+
+        return $this;
+    }
+
+    /**
+     * Get nbConnexion
+     *
+     * @return int
+     */
+    public function getNbConnexion()
+    {
+        return $this->nbConnexion;
+    }
+}
+
